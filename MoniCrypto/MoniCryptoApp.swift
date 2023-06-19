e073772819e18bdf17e17c14f3ec9674c6fe58d9
Mabast O. Hamadamin <mabastu@gmail.com>
2023-06-19 12:58:38 +0300

    Initial Commit

---
+++ MoniCrypto/MoniCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoniCryptoApp.swift
+//  MoniCrypto
+//
+//  Created by Mabast on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoniCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
