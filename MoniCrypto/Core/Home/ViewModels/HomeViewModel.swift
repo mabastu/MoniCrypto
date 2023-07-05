d1a94c6e4e822d4bc2479d768e7b073727b8bd0f
Mabast O. Hamadamin <mabastu@gmail.com>
2023-07-05 17:12:12 +0300

    Search filtering

--- MoniCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ MoniCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,8 +23,26 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins.sink { [weak self] returnedCoins in
+        // Updates all coins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] returnedCoins in
             self?.allCoins = returnedCoins
         }.store(in: &cancellabels)
+    }
+    
+    private func filterCoins(text: String, coins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                   coin.symbol.lowercased().contains(lowercasedText) ||
+                   coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
