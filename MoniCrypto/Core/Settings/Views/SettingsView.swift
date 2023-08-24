911805d8ee4a7072350e36b2b80da24a1c692a41
Mabast O. Hamadamin <mabastu@gmail.com>
2023-08-24 16:15:52 +0300

    Add a Settings screen using a List with Sections

---
+++ MoniCrypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  MoniCrypto
+//
+//  Created by Mabast on 24/08/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
